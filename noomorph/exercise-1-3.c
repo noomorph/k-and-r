378ffb4ec715ac02a05225c937a16d5b368cd03b
Yaroslav Serhieiev <noomorph@gmail.com>
2017-10-11 09:43:22 +0300

    exercise: 1-3, 1-6

--- noomorph/exercise-1-3.c
+++ noomorph/exercise-1-3.c
@@ -1,14 +1,28 @@
 #define DEG (wint_t)176
+#define safe_run(exitcode, expression) if (exitcode == 0) { exitcode = expression(); };
 
 #include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
 
+int repeat(char* s, int count) {
+	for (int i = 0; i < count; i++) {
+		printf("%s", s);
+	}
+}
+
 float convert_to_celsius(int degree_in_fahrengeit) {
 	return 5.0 * (degree_in_fahrengeit - 32.0) / 9.0;
 }
 
-int print_conversion_table() {
+int print_conversion_table_header() {
+	printf("%20s|%20s\n", "Fahrenheit", "Celsius");
+	repeat("=", 41);
+	printf("\n");
+	return 0;
+}
+
+int print_conversion_table_body() {
 	const float lower = 0, upper = 300, step = 20;
 	float fahr = lower, celsius = 0.0;
 
@@ -16,7 +30,7 @@ int print_conversion_table() {
 
 	while (fahr <= upper) {
 		celsius = convert_to_celsius(fahr);
-		printf("%3.0f%lcF\t%6.1f%lcC\n", fahr, DEG, celsius, DEG);
+		printf("%18.0f%lcF|%18.1f%lcC\n", fahr, DEG, celsius, DEG);
 		fahr += step;
 	}
 
@@ -25,5 +39,8 @@ int print_conversion_table() {
 
 int main()
 {
-	return print_conversion_table();
+	int code = 0;
+	safe_run(code, print_conversion_table_header);
+	safe_run(code, print_conversion_table_body);
+	return code;
 }
